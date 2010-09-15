1e50950465d1ddb7d811159f74bfb75c2dcc2fdc
Sam Aaron <samaaron@gmail.com>
2010-09-15 16:41:54 +0100

    add scratch.el and also ignore slime-protocol-version

--- shared/vendored.el
+++ shared/vendored.el
@@ -99,6 +99,7 @@
 (eval-after-load "slime"
   '(progn (slime-setup '(slime-repl))))
 (add-path "vendor/slime")
+(eval-after-load 'slime '(setq slime-protocol-version 'ignore))
 (require 'slime)
 (slime-setup)
 
