a5b88186ccc5da36d89a525d769bda81116b7718
Sam Aaron <samaaron@gmail.com>
2010-10-27 15:50:55 +0100

    update clojure-ascii-art-overlays to be more idomatic to lambda calculus forms

--- starter-kit-lisp.el
+++ starter-kit-lisp.el
@@ -60,16 +60,15 @@
   '(font-lock-add-keywords
     'clojure-mode `(("(\\(fn\\>\\)"
                      (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "ƒ")
+                                               (match-end 1) "λ")
                                nil))))))
 
 (eval-after-load 'clojure-mode
   '(font-lock-add-keywords
     'clojure-mode `(("\\(#\\)("
                      (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "λ")
+                                               (match-end 1) "ƒ")
                                nil))))))
-
 
 
 (provide 'starter-kit-lisp)
