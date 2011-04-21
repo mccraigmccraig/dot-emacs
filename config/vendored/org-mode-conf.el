0894356bc2d88ecdfa307a6e14567de7e77bf5b9
Sam Aaron <samaaron@gmail.com>
2011-04-21 20:34:27 +0100

    fine-tune org-mode extension hooks

--- config/vendored/org-mode-conf.el
+++ config/vendored/org-mode-conf.el
@@ -1,5 +1,5 @@
 ;I currently get strange behavour with org-mode
 (add-path "vendor/org-mode/lisp")
 (add-path "vendor/org-mode/contrib/lisp")
-(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (require 'org-install)
