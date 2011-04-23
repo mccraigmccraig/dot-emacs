a330920c1456ba2ec5a0237a375488092da85792
Sam Aaron <samaaron@gmail.com>
2011-04-23 16:57:18 +0100

    force eval of concat

--- config/core.el
+++ config/core.el
@@ -19,7 +19,7 @@
 (require 'backup-dir)
 (make-variable-buffer-local 'backup-inhibited)
 (setq bkup-backup-directory-info
-      '((t (concat dotfiles-tmp-dir "backups") ok-create full-path prepend-name)))
+      `((t ,(concat dotfiles-tmp-dir "backups") ok-create full-path prepend-name)))
 (setq delete-old-versions t
       kept-old-versions 1
       kept-new-versions 3
