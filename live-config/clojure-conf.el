64d2843001d346e3f0a1e1d1b56feb92cb6a7783
Sam Aaron <samaaron@gmail.com>
2011-05-14 18:15:43 +0200

    Add fancy symbol for #{} set literal to look like: ∈{}

--- live-config/clojure-conf.el
+++ live-config/clojure-conf.el
@@ -17,6 +17,13 @@
                                                (match-end 1) "ƒ")
                                nil))))))
 
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\){"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "∈")
+                               nil))))))
+
 (eval-after-load 'find-file-in-project
   '(add-to-list 'ffip-patterns "*.clj"))
 
