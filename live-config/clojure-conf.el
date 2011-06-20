526c883daa2afcf0717aefd01c999f40d3e5463f
Sam Aaron <samaaron@gmail.com>
2011-06-20 10:35:50 +0100

    remove regexp fancy symbol

--- live-config/clojure-conf.el
+++ live-config/clojure-conf.el
@@ -24,13 +24,6 @@
                                                (match-end 1) "∈")
                                nil))))))
 
-(eval-after-load 'clojure-mode
-  '(font-lock-add-keywords
-    'clojure-mode `(("\\(#\\)\""
-                     (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "§")
-                               nil))))))
-
 (eval-after-load 'find-file-in-project
   '(add-to-list 'ffip-patterns "*.clj"))
 
