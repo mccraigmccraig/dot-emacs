82437a291ccab6942ec6985274a54ff3cecf8ee3
Sam Aaron <samaaron@gmail.com>
2012-01-31 13:16:51 +0000

    load actionscript-config when loading actionscript-mode

--- config/actionscript-conf.el
+++ config/actionscript-conf.el
@@ -3,3 +3,5 @@
 
 (autoload 'actionscript-mode "actionscript-mode" "Major mode for actionscript." t)
 (add-to-list 'auto-mode-alist '("\\.as$" . actionscript-mode))
+
+(eval-after-load	"actionscript-mode" '(load "actionscript-config"))
