831c3fead20d58a0d1c39cff666cfc1166d6fb11
Sam Aaron <samaaron@gmail.com>
2012-02-28 08:43:01 +0000

    auto-join lein irc room

--- config/erc-conf.el
+++ config/erc-conf.el
@@ -9,7 +9,7 @@
 (require 'erc-join)
 (erc-autojoin-mode 1)
 (setq erc-autojoin-channels-alist
-      '(("freenode.net" "#clojure" "#overtone")))
+      '(("freenode.net" "#clojure" "#overtone" "#leiningen")))
 
 (defun erc-connect ()
   "Start up erc and connect to freedonde"
