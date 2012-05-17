3394a293f94223beb860e4d590e6796c2dc9b3a0
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2012-05-17 12:27:05 +0100

    move personal config onto packs

--- init.el
+++ init.el
@@ -65,3 +65,32 @@
 
 ;; Load all packs - Power Extreme!
 (mapcar (lambda (pack-name) (live-load-pack pack-name)) live-packs)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(ns-antialias-text nil)
+ '(sclang-auto-scroll-post-buffer t)
+ '(sclang-eval-line-forward nil)
+ '(sclang-help-path (quote ("/Applications/SuperCollider/Help")))
+ '(sclang-runtime-directory "~/.sclang/")
+ '(tab-width 2)
+ '(term-default-bg-color "#000000")
+ '(term-default-fg-color "#dddd00"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#EDEDED" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "apple" :family "Monaco"))))
+ '(ediff-even-diff-A ((((class color) (background dark)) (:background "dark green"))))
+ '(ediff-even-diff-B ((((class color) (background dark)) (:background "dark red"))))
+ '(ediff-odd-diff-A ((((class color) (background dark)) (:background "dark green"))))
+ '(ediff-odd-diff-B ((((class color) (background dark)) (:background "dark red"))))
+ '(font-lock-comment-face ((t (:foreground "#8B8989"))))
+ '(mumamo-background-chunk-major ((((class color) (background dark)) (:background "black"))))
+ '(mumamo-background-chunk-submode1 ((((class color) (background dark)) (:background "black")))))
--- packs/user/config/personal.el
+++ packs/user/config/personal.el
@@ -1,6 +1,6 @@
 ;;PATH stuff
 (push "/usr/local/bin" exec-path)
-(setq path "/Applications/Supercollider:/Users/sam/bin:/usr/local/bin:$PATH:/bin:/usr/bin")
+(setq path "/Applications/Supercollider:/Users/mccraig/bin:/usr/local/bin:$PATH:/bin:/usr/bin")
 (setenv "PATH" path)
 (push "/Applications/SuperCollider" exec-path)
 
@@ -12,25 +12,140 @@
                          (name . "^\\*scratch\\*$")
                          (name . "^\\*Messages\\*$")
                          (name . "^\\*Completions\\*$")
+                         (name . "^\\*ack\\*$")
+                         (name . "^\\*Help\\*$")
+                         (name . "^\\*Apropos\\*$")
                          (filename . ".emacs.d")))
 
-               ("improcess apps"  (filename . "Development/improcess/apps"))
-               ("improcess lib"   (filename . "Development/improcess/lib"))
                ("dired" (mode . dired-mode))
-
-               ))))
+               ("SLIME" (or
+                         (name . "^\\*inferior-lisp\\*$")
+                         (name . "^\\*slime-events\\*$")
+                         (name . "^\\*slime-repl.*$")
+                         (name . "^\\*sldb clojure/.*$")
+                         (name . "^\\*SLIME Macroexpansion\\*")
+                         (name . "^\\*SLIME Compilation\\*")))))))
 (add-hook 'ibuffer-mode-hook
           (lambda ()
             (ibuffer-switch-to-saved-filter-groups "default")))
 
-
 ;;projects
 (project-def "overtone"
-             '((basedir          "/Users/sam/Development/improcess/lib/overtone")
+             '((basedir          "/Users/mccraig/dev/clojure/overtone")
                (src-patterns     ("*.clj"))
                (ignore-patterns  ("*.log" "*#"))
-               (file-list-cache  "/Users/sam/.emacs.d/tmp/mk-project-cache/overtone-files")
-               (open-files-cache "/Users/sam/.emacs.d/tmp/mk-project-cache/overtone-open-files")
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/overtone-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/overtone-open-files")
+               (vcs              git)
+               (ack-args         "--clojure")
+               (shutdown-hook    nil)))
+
+(project-def "tones"
+             '((basedir          "/Users/mccraig/dev/tones")
+               (src-patterns     ("*.clj"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/tones-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/tones-open-files")
+               (vcs              git)
+               (ack-args         "--clojure")
+               (shutdown-hook    nil)))
+
+(project-def "bcrp"
+             '((basedir          "/Users/mccraig/dev/codeface/bcrp")
+               (src-patterns     ("*.rb" "*.html" "*.css"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/bcrp-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/bcrp-open-files")
+               (vcs              git)
+               (ack-args         "")
+               (shutdown-hook    nil)))
+
+(project-def "techcitymap-api"
+             '((basedir          "/Users/mccraig/dev/techcitymap-api")
+               (src-patterns     ("*.clj" "*.html" "*.css" "*.js"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/techcitymap-api-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/techcitymap-api-open-files")
+               (vcs              git)
+               (ack-args         "--clojure")
+               (shutdown-hook    nil)))
+
+(project-def "techcitymap-admin"
+             '((basedir          "/Users/mccraig/dev/techcitymap-admin")
+               (src-patterns     ("*.rb" "*.html" "*.css"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/techcitymap-admin-app-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/techcitymap-admin-app-open-files")
+               (vcs              git)
+               (ack-args         "")
+               (shutdown-hook    nil)))
+
+(project-def "dyd_diary_2012"
+             '((basedir          "/Users/mccraig/dev/codeface/dyd_diary_2012")
+               (src-patterns     ("*.rb" "*.html" "*.css"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/dyd_diary_2012-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/dyd_diary_2012-open-files")
+               (vcs              git)
+               (ack-args         "")
+               (shutdown-hook    nil)))
+
+(project-def "cloudtimers"
+             '((basedir          "/Users/mccraig/dev/cloudtimers")
+               (src-patterns     ("*.clj"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/cloudtimers-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/cloudtimers-open-files")
+               (vcs              git)
+               (ack-args         "--clojure")
+               (shutdown-hook    nil)))
+
+(project-def "battleships"
+             '((basedir          "/Users/mccraig/dev/clojure/battleships")
+               (src-patterns     ("*.clj"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/battleships-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/battleships-open-files")
+               (vcs              git)
+               (ack-args         "--clojure")
+               (shutdown-hook    nil)))
+
+(project-def "clojureql"
+             '((basedir          "/Users/mccraig/dev/clojure/clojureql")
+               (src-patterns     ("*.clj"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/clojureql-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/clojureql-open-files")
+               (vcs              git)
+               (ack-args         "--clojure")
+               (shutdown-hook    nil)))
+
+(project-def "mdquery"
+             '((basedir          "/Users/mccraig/dev/ruby/mdquery")
+               (src-patterns     ("*.rb" "*.html" "*.css"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/mdquery-app-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/mdquery-app-open-files")
+               (vcs              git)
+               (ack-args         "")
+               (shutdown-hook    nil)))
+
+(project-def "sonar-server"
+             '((basedir          "/Users/mccraig/dev/sonar-server")
+               (src-patterns     ("*.rb" "*.html" "*.css"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/sonar-server-app-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/sonar-server-app-open-files")
+               (vcs              git)
+               (ack-args         "")
+               (shutdown-hook    nil)))
+
+(project-def "incanterize"
+             '((basedir          "/Users/mccraig/dev/incanterize")
+               (src-patterns     ("*.clj"))
+               (ignore-patterns  ("*.log" "*#"))
+               (file-list-cache  "/Users/mccraig/.emacs.d/tmp/mk-project-cache/incanterize-files")
+               (open-files-cache "/Users/mccraig/.emacs.d/tmp/mk-project-cache/incanterize-open-files")
                (vcs              git)
                (ack-args         "--clojure")
                (shutdown-hook    nil)))
